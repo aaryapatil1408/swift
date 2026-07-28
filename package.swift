a723ea740094ed7daae26be30407ab17c871b2c0
aaryapatil1408 <aaryapatil1408@gmail.com>
2026-07-28 21:59:05 +0530

    Add files via upload

---
+++ package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "MyApp",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12)
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/gonzalezreal/swift-markdown-ui",
+            exact: "2.4.2"
+        )
+    ],
+    targets: [
+        .target(
+            name: "MyApp",
+            dependencies: [
+                .product(
+                    name: "MarkdownUI",
+                    package: "swift-markdown-ui"
+                )
+            ]
+        )
+    ]
+)
